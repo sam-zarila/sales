6288249c4b2f224e85f9e7b342589378ed3ee441
bed-com-03-20 <bed-com-03-20@unima.ac.mw>
2025-01-11 16:07:19 +0200

    first ever store

---
+++ lib/products.ts
@@ -0,0 +1,176 @@
+export interface Product {
+  id: string;
+  name: string;
+  image: string;
+}
+
+// Product images were generated with Midjourney
+// and are stored in a public Vercel Blob storage bucket
+export const products: Product[] = [
+  {
+    id: 'ts-01-white',
+    name: 'T-Shirt 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/refs/heads/main/Deez-Hats-Aztek-Snapback-Cap-Curved-Brim-Deez-Hats-2624.png',
+  },
+  {
+    id: 'ts-02-white',
+    name: 'T-Shirt 02',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/refs/heads/main/Deez-Hats-Cowboy-Heritage-Snapback-Cap-Flat-Brim-Deez-Hats-7770.png',
+  },
+  {
+    id: 'hd-01-white',
+    name: 'Hoodie 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1476825-1.png',
+  },
+  {
+    id: 'wb-01-white',
+    name: 'Windbreaker 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1359568-5.png',
+  },
+  {
+    id: 'rc-01-white',
+    name: 'Racing Coat 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1411769-1.png',
+  },
+  {
+    id: 'sh-01-white',
+    name: 'Shorts 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1428694-4.png',
+  },
+  {
+    id: 'sp-01-white',
+    name: 'Sport Pants 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1437370-3.png',
+  },
+  {
+    id: 'sp-06-white',
+    name: 'Sport Pants 06',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1456575-5.png',
+  },
+  {
+    id: 'sk-01-white',
+    name: 'Socks 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1459969-5.png',
+  },
+  {
+    id: 'ts-01-gray',
+    name: 'T-Shirt 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1469491-3.png',
+  },
+  {
+    id: 'ts-02-gray',
+    name: 'T-Shirt 02',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1474731-4.png',
+  },
+  {
+    id: 'hd-01-gray',
+    name: 'Hoodie 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1474799-3.png',
+  },
+  {
+    id: 'wb-01-gray',
+    name: 'Windbreaker 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1475285-1.png',
+  },
+  {
+    id: 'rc-01-gray',
+    name: 'Racing Coat 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1475460-1.png',
+  },
+  {
+    id: 'sh-01-gray',
+    name: 'Shorts 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1476395-1.png',
+  },
+  {
+    id: 'sp-01-gray',
+    name: 'Sport Pants 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1476825-1.png',
+  },
+  {
+    id: 'sp-06-gray',
+    name: 'Sport Pants 06',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1475460-1.png',
+  },
+  {
+    id: 'sk-01-gray',
+    name: 'Socks 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1478400-1.png',
+  },
+  {
+    id: 'ts-01-black',
+    name: 'T-Shirt 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1437370-3.png',
+  },
+  {
+    id: 'ts-02-black',
+    name: 'T-Shirt 02',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1478417-1.png',
+  },
+  {
+    id: 'hd-01-black',
+    name: 'Hoodie 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1359568-5.png',
+  },
+  {
+    id: 'wb-01-black',
+    name: 'Windbreaker 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1411769-1.png',
+  },
+  {
+    id: 'rc-01-black',
+    name: 'Racing Coat 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1339584.5.png',
+  },
+  {
+    id: 'sh-01-black',
+    name: 'Shorts 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/refs/heads/main/1350083-3.png',
+  },
+  {
+    id: 'sp-01-black',
+    name: 'Sport Pants 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/refs/heads/main/1351880-3_4d36dc43-dce7-4206-8fc8-2df68295842a.png',
+  },
+  {
+    id: 'sp-06-black',
+    name: 'Sport Pants 06',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1359568-5.png',
+  },
+  {
+    id: 'sk-01-black',
+    name: 'Socks 01',
+    image:
+      'https://raw.githubusercontent.com/Bed-com-03-20/asset-s/41a8815a457eaf50c55ba1ffd41754e67ca68732/1475460-1.png',
+  },
+];
+
+export function getProductById(id: string): Product | undefined {
+  return products.find((product) => product.id === id);
+}
